fb87282bc1528c117418b143014d4fd9045f1a97
Cyril Lay <cyril31180@hotmail.fr>
2020-03-04 23:58:54 +0100

    Fixing issues after tensorflow update

--- run_keras_server.py
+++ run_keras_server.py
@@ -1,58 +1,94 @@
-#!/usr/bin/env python3
-
-from keras.applications import ResNet50, InceptionV3
+# import the necessary packages
+from keras.applications.resnet_v2 import ResNet50V2
+from keras.applications.resnet import ResNet50
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
-from keras.applications import xception
+from tensorflow.python.keras.backend import set_session
+from keras.backend import get_session
 from PIL import Image
 import numpy as np
 import flask
 import io
+import tensorflow as tf
 from flask_cors import CORS
 
-from InceptionV3Wrapper import InceptionV3Wrapper
-
+# initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
 CORS(app)
 model = None
+graph = None
+session = None
 
-def load_InceptionV3():
+
+def load_model():
     global model
-    model = InceptionV3Wrapper()
-    model.load_model()
+    global graph
+    global session
+    session = get_session()
+    init = tf.global_variables_initializer()
+    session.run(init)
+    graph = tf.get_default_graph()
+    model = ResNet50(weights="imagenet")
+    model._make_predict_function()
 
-# def load_ResNet50():
-#     global model
-#     model = InceptionV3Wrapper()
-#     model.load_model()
+
+def prepare_image(image, target):
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    image = image.resize(target)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = imagenet_utils.preprocess_input(image)
+    return image
+
 
 @app.route("/test", methods=["GET"])
 def testing():
-    return(flask.jsonify("Test Get Request without"))
+    return flask.jsonify("Hello !")
+
 
 @app.route("/predict", methods=["POST"])
-def return_predictions():
-    predictions = {}
+def predict():
+    # initialize the data dictionary that will be returned from the
+    # view
+    data = {"success": False}
     # ensure an image was properly uploaded to our endpoint
     if flask.request.method == "POST":
-        print("Received ", len(flask.request.files), " files. Processing...")
-        # flask.request.files.items()
+        print(flask.request.files)
+        print(len(flask.request.files))
+        for k, v in flask.request.files.items():
+            print(k, v)
         if flask.request.files.get("image"):
             image = flask.request.files["image"].read()
             image = Image.open(io.BytesIO(image))
-            image = model.prepare_image(image)
-            predictions = model.predict(image)
+            processed_image = prepare_image(image, target=(224, 224))
+            print(f"Image size : {processed_image.shape}")
+            global graph
+            global session
+            preds = None
+            with graph.as_default():
+                set_session(session)
+                preds = model.predict(processed_image)
+            results = imagenet_utils.decode_predictions(preds)
+            data["predictions"] = []
+
+            # loop over the results and add them to the list of
+            # returned predictions
+            for (imagenetID, label, prob) in results[0]:
+                r = {"label": label, "probability": float(prob)}
+                data["predictions"].append(r)
+
+            # indicate that the request was a success
+            data["success"] = True
 
     # return the data dictionary as a JSON response
-    response = flask.jsonify(predictions)
-    response.headers.add('Access-Control-Allow-Origin', '*')	
-    return response
+    response = flask.jsonify(data)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return flask.jsonify(data)
 
+# if this is the main thread of execution first load the model and
+# then start the server
 if __name__ == "__main__":
-    print(("* Loading Keras model and Flask starting server..."
-        "please wait until server has fully started"))
-    # load_ResNet50()
-    load_InceptionV3()
+    print("Loading Keras model starting Flask server...")
+    load_model()
     app.run(host='0.0.0.0')
-
-# Credits to : https://blog.keras.io/building-a-simple-keras-deep-learning-rest-api.html
