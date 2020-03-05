e6de83107f0a2376f3b743e0c26fa64d5938385c
Cyril Lay <cyril31180@hotmail.fr>
2020-03-05 12:45:49 +0100

    Small refactor and cleaning

--- run_keras_server.py
+++ run_keras_server.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from keras.applications.resnet_v2 import ResNet50V2
 from keras.applications.resnet import ResNet50
 from keras.preprocessing.image import img_to_array
@@ -12,12 +11,11 @@ import io
 import tensorflow as tf
 from flask_cors import CORS
 
-# initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
 CORS(app)
-model = None
-graph = None
 session = None
+graph = None
+model = None
 
 
 def load_model():
@@ -29,6 +27,7 @@ def load_model():
     session.run(init)
     graph = tf.get_default_graph()
     model = ResNet50(weights="imagenet")
+    # https://github.com/keras-team/keras/issues/6124
     model._make_predict_function()
 
 
@@ -39,6 +38,7 @@ def prepare_image(image, target):
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
     image = imagenet_utils.preprocess_input(image)
+    print(f"Converted image to shape : {image.shape}")
     return image
 
 
@@ -49,46 +49,33 @@ def testing():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    # initialize the data dictionary that will be returned from the
-    # view
-    data = {"success": False}
-    # ensure an image was properly uploaded to our endpoint
+    """
+    Returns a JSON response containing the classification labels predicted by the neural network for the posted image
+    files
+
+    Usage : `curl -X POST -F image=@YOUR_IMAGE.JPG http://0.0.0.0:5000/predict`
+    """
+    data = {"success": False, "predictions": []}
     if flask.request.method == "POST":
-        print(flask.request.files)
-        print(len(flask.request.files))
-        for k, v in flask.request.files.items():
-            print(k, v)
+        print(f"Received {len(flask.request.files)} files.")
         if flask.request.files.get("image"):
             image = flask.request.files["image"].read()
             image = Image.open(io.BytesIO(image))
             processed_image = prepare_image(image, target=(224, 224))
-            print(f"Image size : {processed_image.shape}")
-            global graph
-            global session
-            preds = None
             with graph.as_default():
                 set_session(session)
                 preds = model.predict(processed_image)
             results = imagenet_utils.decode_predictions(preds)
-            data["predictions"] = []
-
-            # loop over the results and add them to the list of
-            # returned predictions
-            for (imagenetID, label, prob) in results[0]:
+            for (_, label, prob) in results[0]:
                 r = {"label": label, "probability": float(prob)}
                 data["predictions"].append(r)
-
-            # indicate that the request was a success
             data["success"] = True
-
-    # return the data dictionary as a JSON response
     response = flask.jsonify(data)
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add("Access-Control-Allow-Origin", "*")
     return flask.jsonify(data)
 
-# if this is the main thread of execution first load the model and
-# then start the server
+
 if __name__ == "__main__":
     print("Loading Keras model starting Flask server...")
     load_model()
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
